e01f7ef2a1284efd8ddbc1e56c85019034b66cee
Notebook <Notebook@Docente-111>
2015-04-04 01:06:34 -0300

    Finalizo primera etapa de prototipado de aplicacion de agenda

--- apps/eventmachine/modules/agenda/templates/indexSuccess.php
+++ apps/eventmachine/modules/agenda/templates/indexSuccess.php
@@ -1,28 +1,47 @@
 <?php use_stylesheet('fullcalendar.min.css') ?>
 <?php use_javascript('fullcalendar.min.js') ?>
 
-
 <div class="row">
     <div class="col-md-9">
         <div id="calendar"></div>
     </div>
-  <div class="col-md-3">
-      <div id="eventTicker">
-        <div>Event 1</div>
-        <div>Event 2</div>
-        <div>Event 3</div>
+    <div class="col-md-3">
+        
+        <div id="event-form-div">
+            <!-- aca iria el formulario de nuevo evento -->
+        </div>
+        
+        <div id="ticker" class="list-group">
+            <div class="list-group">
+                <a href="#" class="list-group-item">
+                  <h4 class="list-group-item-heading">List group item heading</h4>
+                  <p class="list-group-item-text">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
+                </a>
+                <a href="#" class="list-group-item">
+                  <h4 class="list-group-item-heading">List group item heading</h4>
+                  <p class="list-group-item-text">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
+                </a>
+                <a href="#" class="list-group-item">
+                  <h4 class="list-group-item-heading">List group item heading</h4>
+                  <p class="list-group-item-text">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
+                </a>
+            </div>
+        </div>
     </div>
-  </div>
 </div>
 
 
 <script type="text/javascript">
     $(document).ready(function() {
-        // page is now ready, initialize the calendar...
+        // initialize the calendar...
 
-        $('#calendar').fullCalendar({
+        $("#calendar").fullCalendar({
             // put your options and callbacks here
         })
+        
+        // initialize the event scroller
+//        $("#eventTicker").jscroll();
+        
 
     });
 </script>
