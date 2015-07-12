3bcaa539c033c24f7f1e49adf581e0dc094b8ae1
Notebook <Notebook@Docente-111>
2015-07-12 20:22:51 -0300

    Mejora: Agrego opciones de filtrado por fecha (mes, año) y por fuente del evento. Requiere implementacion

--- apps/eventmachine/modules/agenda/templates/_agenda.php
+++ apps/eventmachine/modules/agenda/templates/_agenda.php
@@ -30,13 +30,42 @@
             title: "Agrega un nuevo evento al calendario"
         });
        
-        var searchButton = $("<input type='text' id='filterBtn' class='form-control input-sm' placeholder='Filtrar eventos'>");
+        var filterInput = $("<input>")
+                .attr({
+                    'type':  'text',
+                    'id':    'filterBtn',
+                    'class': 'form-control',
+                    'placeholder': 'Filtrar eventos'
+                });
+        
+        var searchDropdown = $("<div class=\"input-group-btn\">")
+                .append("<button type=\"button\" class=\"btn btn-default dropdown-toggle\" data-toggle=\"dropdown\" aria-haspopup=\"true\" aria-expanded=\"false\">" +
+                    "<span class=\"glyphicon glyphicon-filter\" aria-hidden=\"true\"></span>" +
+                "</button>")
+                .append(
+                    "<ul class=\"dropdown-menu dropdown-menu-right\">" +
+                        "<li class=\"dropdown-header\">Tiempo</li>" +
+                        "<li><a href=\"#\">Este Mes</a></li>" +
+                        "<li><a href=\"#\">Este Año</a></li>" +
+//                        "<li role=\"separator\" class=\"divider\"></li>" +
+                        "<li class=\"dropdown-header\">Fuente</li>" +
+                        "<li><a href=\"#\">De Facebook</a></li>" +
+                        "<li><a href=\"#\">De GCalendar</a></li>" +
+                    "</ul>"
+                );        
+        
+        var searchDiv = $("<div class=\"form-group\">");
+        
+        $("<div class=\"input-group input-group-sm\">")
+            .append(filterInput)
+            .append(searchDropdown)
+            .appendTo(searchDiv);
         
         // Agrego div con los nuevos botones
 
         $("div.fc-toolbar > div.fc-right")
             .append(createDiv)
-            .append(searchButton)
+            .append(searchDiv)
             .addClass('form-inline');
 
 
@@ -49,7 +78,7 @@
                 eraseEventUrl: "<?php echo url_for("@borrar_evento_ajax")?>",
             }).sieve({ 
                 itemSelector: "a.list-group-item",
-                searchInput: searchButton
+                searchInput: filterInput
             });
         
 
